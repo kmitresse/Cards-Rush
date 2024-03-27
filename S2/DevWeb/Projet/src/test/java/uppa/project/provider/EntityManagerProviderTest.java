5d33d33a08c0f6da2cf7f1b4daf68e85b88b6092
Lucàs <vabrelucas@gmail.com>
2024-03-27 22:57:04 +0100

    test(DevWeb): Add tests for EntityManagerProvider

--- S2/DevWeb/Projet/src/test/java/uppa/project/provider/EntityManagerProviderTest.java
+++ S2/DevWeb/Projet/src/test/java/uppa/project/provider/EntityManagerProviderTest.java
@@ -1,20 +1,33 @@
 package uppa.project.provider;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EntityManagerProviderTest {
 
+  @BeforeAll
+  static void setUp() {
+    EntityManagerProvider.setPersitenceUnitName("test");
+  }
+
   @Test
   void getInstance() {
-    // TODO Implement this method
-    fail();
+    EntityManager entity1 = EntityManagerProvider.getInstance();
+    EntityManager entity2 = EntityManagerProvider.getInstance();
+
+    assertNotNull(entity1);
+    assertNotNull(entity2);
+    assertEquals(entity1, entity2);
   }
 
   @Test
   void close() {
-    // TODO Implement this method
-    fail();
+    EntityManager entity = EntityManagerProvider.getInstance();
+    EntityManagerProvider.close();
+
+    assertFalse(entity::isOpen);
   }
 }
