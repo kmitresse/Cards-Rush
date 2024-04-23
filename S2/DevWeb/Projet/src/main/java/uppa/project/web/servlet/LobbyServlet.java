46764fee999f7d2068d10e2e210326224cfb49e2
Lucàs <vabrelucas@gmail.com>
2024-04-23 14:59:01 +0200

    feat(DevWeb): Add Lobby page

---
+++ S2/DevWeb/Projet/src/main/java/uppa/project/web/servlet/LobbyServlet.java
@@ -0,0 +1,35 @@
+/*
+ * MainMenuServlet.java, 20/03/2024
+ * UPPA M1 TI 2023-2024
+ * Pas de copyright, aucun droits
+ */
+
+package uppa.project.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import uppa.project.database.pojo.Game;
+import uppa.project.database.pojo.Player;
+import uppa.project.database.pojo.User;
+
+@WebServlet(name = "lobbyServlet", value = "/lobby")
+public class LobbyServlet extends HttpServlet {
+
+  public void init() {
+  }
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    request.getRequestDispatcher("/WEB-INF/pages/lobby.jsp").forward(request, response);
+  }
+
+  public void destroy() {
+  }
+}
---
+++ S2/DevWeb/Projet/src/main/webapp/WEB-INF/pages/lobby.jsp
@@ -0,0 +1,24 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@taglib prefix="layout" tagdir="/WEB-INF/tags/layouts" %>
+<%@taglib prefix="component" tagdir="/WEB-INF/tags/components" %>
+
+
+<layout:base>
+    <component:hero>
+        <div class="columns">
+            <div class="column">
+                <component:card title="Menu principal">
+                    <ul>
+                        <li><a href="${pageContext.request.contextPath}/new">Nouvelle partie</a></li>
+                        <li><a href="${pageContext.request.contextPath}/join">Rejoindre une partie</a></li>
+                    </ul>
+                </component:card>
+            </div>
+            <div class="column">
+                <component:card>
+                    <component:connected-user-list/>
+                </component:card>
+            </div>
+        </div>
+    </component:hero>
+</layout:base>
